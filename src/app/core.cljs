e8a87b21d6c3deec31fb1c849efadc8358e49984
Jan Hein Hoogstad <yeehaa@offcourse.io>
2016-07-04 23:27:20 +0200

    initial commit

---
+++ src/app/core.cljs
@@ -0,0 +1,29 @@
+(ns app.core
+  (:require [cljs.nodejs :as node]
+            [cljs.core.async :refer [<! chan >!]])
+  (:require-macros [cljs.core.async.macros :refer [go]]))
+
+(def AWS (node/require "aws-sdk"))
+
+(node/enable-util-print!)
+;; const payload = new Buffer(record.kinesis.data, 'base64').toString('ascii');
+
+(defn convert-payload [data]
+  (-> js/JSON
+      (.parse (.toString (js/Buffer. data "base64") "ascii"))
+      (js->clj :keywordize-keys true)))
+
+(defn ^:export handler [event context cb]
+  (let [event (js->clj event :keywordize-keys true)]
+    (println (map #(-> %
+                       first
+                       second
+                       :data
+                       convert-payload
+                       :url)
+                  (:Records event)))
+    (go
+      (cb nil (clj->js event)))))
+
+(defn -main [] identity)
+(set! *main-cli-fn* -main)
