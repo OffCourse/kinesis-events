179555071e6c23c8b58550c1bfd48792bd686492
Jan Hein Hoogstad <yeehaa@offcourse.io>
2016-07-05 14:07:11 +0200

    add url resolution

--- src/app/core.cljs
+++ src/app/core.cljs
@@ -4,26 +4,48 @@
   (:require-macros [cljs.core.async.macros :refer [go]]))
 
 (def AWS (node/require "aws-sdk"))
+(def expander (node/require "unshortener"))
+#_(def expander (node/require "expand-url"))
+(def exp (node/require "node-url-expand"))
 
 (node/enable-util-print!)
-;; const payload = new Buffer(record.kinesis.data, 'base64').toString('ascii');
 
 (defn convert-payload [data]
   (-> js/JSON
       (.parse (.toString (js/Buffer. data "base64") "ascii"))
       (js->clj :keywordize-keys true)))
 
+(defn extract-payload [event]
+  (-> (:Records event)
+      first
+      first
+      second
+      :data))
+
+(defn expand-url [res]
+  (let [{:keys [hostname pathname] :as url} (js->clj (->> res
+                                                   (.stringify js/JSON)
+                                                   (.parse js/JSON))
+                                             :keywordize-keys true)]
+    (str hostname pathname)))
+
+(defn extract-url [{:keys [url] :as record}]
+  (let [c (chan)]
+    (.expand expander url #(go (if %1
+                                 (println %1)
+                                 (>! c (expand-url %2)))))
+    c))
+
 (defn ^:export handler [event context cb]
-  (let [event (js->clj event :keywordize-keys true)
-        urls (map #(-> %
-                       first
-                       second
-                       :data
-                       convert-payload
-                       :url)
-                  (:Records event))]
-    (go
-      (cb nil (clj->js urls)))))
+  (go
+    (let [event (js->clj event :keywordize-keys true)
+          payload (-> event
+                      (extract-payload)
+                      (convert-payload))
+          url     (<! (extract-url payload))
+          url-string (.stringify js/JSON (clj->js {:url url}))]
+      (println url-string)
+      (cb nil url-string))))
 
 (defn -main [] identity)
 (set! *main-cli-fn* -main)
